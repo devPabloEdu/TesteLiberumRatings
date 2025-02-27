3d390551b7366ac5006a931870491bb5145224a7
Pablo <vilelapege1113@gmail.com>
2025-02-26 22:13:25 -0300

    Metodo Post e get funcionando e testados

---
+++ Backend/ApiEmpresas/Controllers/EmpresasController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiEmpresas.Data;
+using ApiEmpresas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmpresas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmpresasController : ControllerBase
+    {
+        private readonly EmpresasContext _context;
+
+        public EmpresasController (EmpresasContext context)
+        {
+            _context = context;
+        }
+
+        //Listar todas as empresas criadas
+        [HttpGet]
+        public async Task<ActionResult<List<EmpresaModel>>> ListarEmpresas()
+        {
+            var empresas = await _context.Empresa.ToListAsync();
+            return Ok(empresas);
+        }
+
+        //Listar uma empresa especifica, ao buscar por seu ID
+        [HttpGet ("{Id}")]
+        public async Task<ActionResult<EmpresaModel>> VerificarEmpresaEspecifica(int Id)
+        {
+            var empresaEspecifica = await _context.Empresa.FindAsync(Id);
+            
+            if (empresaEspecifica == null)
+            {
+                return NotFound("Id da empresa informada é inexistente");
+            }
+
+            return Ok(empresaEspecifica);
+        }
+
+        //Criar uma empresa
+        [HttpPost]
+        public async Task<ActionResult<EmpresaModel>> CriarEmpresa(EmpresaModel empresa)
+        {
+            //metodos para verificar se o cnpj e a razão social já são existentes
+            if(_context.Empresa.Any(e => e.Cnpj == empresa.Cnpj))
+            {
+                return BadRequest("Já existe uma empresa com esse CNPJ");
+            }
+
+            if(_context.Empresa.Any(e => e.RazaoSocial == empresa.RazaoSocial))
+            {
+                return BadRequest("Já existe em sistema uma empresa com essa mesma razão social");
+            }
+
+            empresa.CriadoEm = DateTime.Now;
+            empresa.AtualizadoEm = DateTime.Now;
+
+            _context.Empresa.Add(empresa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(VerificarEmpresaEspecifica),new {id = empresa.Id}, empresa);
+        }
+
+        
+    }
+}
---
+++ Backend/ApiEmpresas/Migrations/20250227003638_CriarBancoCadastroEmpresas.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiEmpresas.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarBancoCadastroEmpresas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RazaoSocial = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Cnpj = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    DataDeRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AtualizadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresa_Cnpj",
+                table: "Empresa",
+                column: "Cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresa_RazaoSocial",
+                table: "Empresa",
+                column: "RazaoSocial",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empresa");
+        }
+    }
+}
